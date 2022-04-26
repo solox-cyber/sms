e24e02ac2dbbd377df086539e6ba6eeea307d73f
Osadiaye Solomon <srose033@gmail.com>
2022-04-26 09:01:22 +0100

    add new files

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
+use App\Http\Controllers\StudentController;
 
 class StudentController extends Controller
 {
     //
+    public function index(){
+        $students = Student::all();
+        return view('student',['$students'=>$students, 'layout'=>'index']);
+    }
+
+    public function create(){
+        $students = Student::all();
+        return view('student',['students'=>$students,'layout'=>'create']);
+    }
+
+    public function store(Request $request){
+        $students = new Student();
+        $student->cne = $request->input('cne');
+        $student->firstName = $request->input('firstName');
+        $student->secondName = $request->input('secondName');
+        $student->age = $request->input('age');
+        $student->speciality = $request->input('speciality');
+        $student->save();
+        return redirect('/');
+    }
+
+    public function show($id){
+      $student = Student::find($id);
+      $students = Student::all();
+      return view('student',['$students'=>$students,'$student'=>$student,'layout'=>'show']);
+    }
+
+    public function edit($id){
+        $student = Student::find($id);
+        $students = Student::all();
+        return view('student',['$students'=>$students,'$student'=>$student,'layout'=>'edit']);
+    }
+
+    public function update(Request $request, $id){
+        $student = Student::find($id);
+        $student->cne = $request->input('cne');
+        $student->firstName = $request->input('firstName');
+        $student->secondName = $request->input('secondName');
+        $student->age = $request->input('age');
+        $student->speciality = $request->input('speciality');
+        $student->save();
+        return redirect('/');
+    }
+
+    public function destory($id){
+        $student = Student::find($id);
+        $student->delete();
+        return redirect('/');
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/',"App\Http\Controllers\StudentController@index");
+Route::get('/edit/{id}',"StudentController@edit");
+Route::get('/show/{id}',"StudentController@show");
+Route::get('/create',"StudentController@create");
+Route::get('/store',"StudentController@store");
+Route::get('/update/{id}',"StudentController@update");
